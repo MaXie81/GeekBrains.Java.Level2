4e4cf373786a0bf9021f0e5b42d908a863607720
MaXie81 <maxie81@yandex.ru>
2021-02-20 11:56:35 +0300

    Практические задания. Урок 1

---
+++ lesson1/runner/Robot.java
@@ -0,0 +1,8 @@
+package runner;
+
+public class Robot extends Runner {
+    final public static String[] ARR_NAME = {"Вертер", "Классик", "C-3po", "Дроид", "R2-D2"};
+    public Robot(String name, float l, float h) {
+        super("Робот", name, l, h);
+    }
+}
