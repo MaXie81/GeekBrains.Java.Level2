86843f094864cb08c0fd97fe3cdf8704e25979b8
MaXie81 <maxie81@yandex.ru>
2021-03-15 20:12:27 +0300

    Практические задания. Урок 8

---
+++ lesson8/ChatMessageService.java
@@ -0,0 +1,55 @@
+package network;
+
+import java.io.IOException;
+
+/**
+ * Project java_core_l2
+ *
+ * @Author Alexander Grigorev
+ * Created 08.03.2021
+ * v1.0
+ *
+ * Реализация сервиса работы с сообщениями по сети
+ * Обрабатывает получение и отправку сообщений
+ */
+public class ChatMessageService implements MessageService {
+    private String host;
+    private int port;
+    private NetworkService networkService;
+    private MessageProcessor processor = null;
+
+    public ChatMessageService(String host, int port, MessageProcessor processor) {
+        this.host = host;
+        this.port = port;
+        this.processor = processor;
+//        connectToServer();
+    }
+
+    @Override
+    public void connectToServer() {
+        try {
+            networkService = new NetworkService(host, port, this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void disconnectFromServer() {
+        networkService.closeConnection();
+    }
+
+    @Override
+    public void sendMessage(String msg) {
+        networkService.writeMessage(msg);
+    }
+
+    /**
+     * тут как раз используется процессор, чтобы наш сервис знал, кому же отдать полученное сообщение
+     * @param msg
+     */
+    @Override
+    public void receiveMessage(String msg) {
+        processor.processMessage(msg);
+    }
+}
---
+++ lesson8/MessageService.java
@@ -0,0 +1,16 @@
+package network;
+
+/**
+ * Project java_core_l2
+ *
+ * @Author Alexander Grigorev
+ * Created 08.03.2021
+ * v1.0
+ * Абстракция для сервиса работы с сообщениями
+ */
+public interface MessageService {
+    void sendMessage(String msg);
+    void receiveMessage(String msg);
+    void connectToServer();
+    void disconnectFromServer();
+}
